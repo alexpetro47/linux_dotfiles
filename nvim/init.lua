7d2d7aa1a0ea4a0ef3beb29b29e9712e8cf06a70
Alex Petro <alexmpetro@gmail.com>
2024-11-06 12:48:43 -0800

    standard commit message

--- nvim/init.lua
+++ nvim/init.lua
@@ -95,7 +95,7 @@ vim.keymap.set('n', '<leader>n', ':bn<CR>', {desc = 'next buffer'}) --next buffe
 vim.keymap.set('n', '<leader>b', ':buffers<CR>', {desc = 'list buffers'}) --list buffers
 vim.keymap.set('n', '<leader>t', ':lcd %:p:h<CR> <C-w>v<C-w>l :term<CR>a', {desc = 'terminal'}) --terminal
 vim.keymap.set("n", "<leader>D", ":w<CR>:! clang++ -std=c++14 -g -o %:r %<CR>:cd %:p:h<CR>:pwd<CR> <C-w>v<C-w>l :term lldb %:r<CR>a ", {noremap = true, silent = true, desc = 'Debug'}) --debugging
-vim.keymap.set('n', '<leader>c', ':! clang++ -std=c++14 -Wall -g -o %:r %<CR>', {desc = 'clang++ compile w. debug'}) -- c++ compile w clang
+vim.keymap.set('n', '<leader>c', ':! clang++ -std=c++14 -fstandalone-debug -Wall -g -o %:r %<CR>', {desc = 'clang++ compile w. debug'}) -- c++ compile w clang
 vim.keymap.set('n', '<leader>L', ':let @" = expand("%")<CR>', { noremap = true, silent = true, desc = 'get path to current file'}) --get path to current file
 vim.keymap.set("v", "<leader>W", [[:s/\S\+//gn<CR>]], {noremap = true, silent = true, desc = 'word count in selection'}) --
 vim.keymap.set("n", "<leader>W", [[:%s/\S\+//gn<CR>]], {noremap = true, silent = true, desc = 'word count in file'}) --
