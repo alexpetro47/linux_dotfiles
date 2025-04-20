098badd86e75818ed4b7f9d25f440902d5839f90
Alex Petro <alexmpetro@gmail.com>
2025-04-20 15:31:02 -0700

    standard commit message

--- i3/load_screen_layout.sh
+++
@@ -1,9 +0,0 @@
-# Check for HDMI connection on either HDMI-1 or HDMI-2
-if xrandr | grep -q "HDMI-[12] connected"; then
-    # If HDMI is connected on HDMI-1 or HDMI-2, load the HDMI layout
-    ~/.screenlayout/external_monitor.sh
-else
-    # If no HDMI connection, load the laptop-only layout
-    ~/.screenlayout/laptop_only.sh
-fi
-
