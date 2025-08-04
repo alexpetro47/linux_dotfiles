a839d24571ff8b503994d386cfd667db1c69d1c9
Alex Petro <alexmpetro@gmail.com>
2025-08-04 10:39:27 -0700

    thinkpad

--- new-machine-setup/other-installs.sh
+++ new-machine-setup/other-installs.sh
@@ -2,6 +2,7 @@
 #!/bin/bash
 
 # top portion is commented out as alr used in init-guide
+# see all installs you've made via `apt-mark showmanual`
 apt_apps=(
   # google-chrome
   # git 
@@ -14,36 +15,43 @@ apt_apps=(
   # rofi
   # feh
   # picom
- 
   mintupgrade
-
   fzf
   nemo
   fd-find
   ripgrep
-
   lf
   btop
   ranger
   trash-cli
-
   flameshot
   simplescreenrecorder
   playerctl
   vlc
-
   pipes-sh
   cbonsai
-
   lldb
   clang
   python3
   pylint
-
   snap
   xclip
   xdotool
+  dbeaver
+  curl
+  zip
+  wget
+  rclone
+  qjackctl
+  pipx
+  neofetch
+  jq
+  jupyter
+  flatpak
+  cloudflared
 )
+
+# need to find latex installs as well
 
 for app in "${apt_apps[@]}"; do
     echo "Installing $app..."
